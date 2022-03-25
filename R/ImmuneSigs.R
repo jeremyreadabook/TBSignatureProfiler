ab6aa80c0121e0c1e42e18e1a5e24506d5f76b88
anirue <86725767+anirue@users.noreply.github.com>
2022-03-25 14:06:31 -0400

    Add files via upload

---
+++ R/ImmuneSigs.R
@@ -0,0 +1,108 @@
+# Create signature
+ImmuneSignatures <- list(DendriticTB_Chaussabel_Blood_200_DN = c("AASS", "ACP2", "ACSL1", "ADA", "ADRB2", "AGRN", "AK4", "ALDH1A2", "AMHR2", "ANKRD12", "ARL4A", "ATF5", "ATG4A", "ATN1", "ATP2A2", "ATP6V1E1", "AURKB", "BRCA2", "BST2", "C3AR1", "CASP1", "CCL3", "CCL4", "CCL8", "CCNA1", "CCR7", "CD163", "CD38", "CD44", "CD80", "CD83", "CDH1", "CEACAM5", "CHST7", "CKAP4", "COBL", "COL5A2", "CPNE6", "CRLF3", "CSF2RB", "CUL1", "CYB5A", "CYRIA", "DLEC1", "DNAJB1", "DUSP5", "DYNLL1", "EFHD1", "EHBP1", "ELL2", "EPOR", "ERC2-IT1", "F13B", "FCGR2A", "FCGR3B", "FGFR2", "FICD", "FSCN1", "G0S2", "GADD45A", "GBP1", "GBP2", "GBX2", "GH1", "GLUL", "GNA12", "GNRH2", "GP1BA", "GPD2", "GREM1", "GSR", "H2AC16", "H2BC21", "HESX1", "HSF4", "HSPA8", "ICAM1", "IER3", "IFI44", "IFI6", "IFIT2", "IFITM2", "IFNG", "IFRD1", "IL10", "IL1RN", "IL2RG", "IQSEC2", "IRF9", "KARS1", "KCNJ10", "KEL", "KRT86", "KYNU", "L3MBTL1", "LBP", "LCP2", "LDHA", "LHFPL2", "LILRB2", "LITAF", "LRP8", "LYN", "LYPD3", "MAGEA5", "MANF", "MAP2K3", "MARCO", "MCCC2", "MFAP3", "MMP12", "MMP7", "MMP9", "MSC", "MT1B", "MT1G", "MT1H", "MT2A", "MT3", "MUC4", "MUC6", "NCALD", "NDRG1", "NFKBIA", "NR4A3", "NRP2", "OAS2", "OBSL1", "OLR1", "OMD", "PARP2", "PCDHGA8", "PDPN", "PFKFB3", "PLAAT4", "PLXNC1", "PPARD", "PPP1R3C", "PSMA5", "PTK2B", "RARRES1", "RASGRP1", "RBMS1", "RELB", "RGS1", "RNF19B", "RRH", "RSC1A1", "RUNX3", "S100A11", "S100A8", "S100A9", "SBNO2", "SCARB2", "SEC61B", "SERPINH1", "SIK3", "SKI", "SLC2A5", "SLPI", "SMURF2", "SNTB2", "SOX9", "SPINK4", "SQSTM1",  "SRGN", "SRSF2",  "SRSF3", "SSB", "STAT2", "STAT3", "STK4", "STX11", "SYMPK", "TAPBP", "TGM3", "TNFAIP2", "TNFSF10", "TNIK", "TNIP1", "TOM1", "TRAF1", "TRAFD1", "TRIB1", "TRIP4", "TSFM", "TUBB", "TUBB2A", "TUBB3", "TXN", "TYMP", "VAMP5", "VCAN", "VEGFA", "WARS1", "WBP2", "WIPI1", "WSCD2", "WTAP", "ZNF785"),
+                         DendriticTB_Chaussabel_Blood_200_UP = c("ABCC5", "ACOT7", "ACY1", "ADAM15", "ADNP", "ADORA3", "AHCY", "ALDH1A1", "ANP32A", "APOC1", "ARHGAP4", "ARHGEF18", "ASTN1", "ASXL1", "ATP11B", "ATP5MC3", "ATP8A1", "ATPAF2", "ATXN2", "BAAT", "BTF3", "CALB2", "CAMK1", "CAMTA2", "CASP9", "CCNH", "CCT4", "CD2AP", "CD33", "CD9", "CDC5L", "CDK10", "CEBPG", "CHN2", "CLEC10A", "COMT", "COX7C", "CPVL", "CYFIP1", "DAB2", "DCAF8", "DCTN2", "DDAH2", "DDX39A", "DDX42", "DNPEP", "DOCK3", "EEF1B2", "EIF2B4", "EIF3H", "EIF3M", "EIF4B", "ERF", "ESRRA", "EVI2B", "F13A1", "FADD", "FAM131A", "FOLH1", "FOXN3", "FOXO3", "GAS7", "GGA2", "GGA3", "GIT2", "GORASP2", "GTF3C2", "GUCA1A", "H2AZ1", "H2AZ2", "HDAC3", "HEBP2", "HEXB", "HNRNPH1", "HOMER3", "HSF1", "ICAM3", "IDH3A", "IER2", "INPP1", "INPP5D", "ISG20L2", "ITGA3", "ITGB5", "ITPKB", "JUND", "KAT2A", "KDELR1", "KRT16", "KXD1", "LAMB2", "LDHB", "LILRA2", "LPXN", "LTA4H", "LY86", "LYL1", "MACROH2A1", "MAN2A2", "MAPK9", "MCM3AP", "MCM6", "MDH1", "MEGF9", "MFN2", "MKNK1", "MYO1F", "NACA", "NARS1", "NCF4", "NDUFS6", "NHP2", "NIPSNAP2", "P2RY11", "P2RY14", "PARP1", "PECAM1", "PEPD", "PFKFB1",  "PFKL", "PGM1", "PIK3CG", "PIP4K2A", "PITPNM1", "PLCB2", "POM121", "PRMT1", "PSEN2", "PTGS1", "PTP4A2", "PYGL", "RBL2", "RERE", "RETREG3", "RGS14", "RNF167", "RNF187", "RNH1", "RPL3", "RPL36A", "RPP38", "RPS27A", "S100A4", "SAMM50", "SARS1", "SDC3", "SDHA", "SEPTIN9", "SGSM2", "SH3BGR", "SIPA1L3", "SIVA1", "SLC1A5", "SLC25A11", "SLC38A10", "SLC7A8", "SMARCD2", "SMARCE1", "SNAPC2", "SNU13", "SOCS1", "SP3", "SPARC", "SPHK2", "SPTAN1", "SRSF6", "SSRP1", "STARD3", "STAT6", "SUGP2", "SUN2", "SYT17", "TAB1", "TARDBP", "TBC1D22A", "THAP12", "TIMP2", "TLE4", "TMC6", "TNFRSF10B", "TOP2B", "TP53", "TRIO", "TUSC2", "UBA2", "UBN1", "UPF3A", "UQCRC1", "USF2", "VPS52", "WDR43", "WDR7", "XPOT", "YWHAH", "ZDHHC3", "ZFP36L2", "ZMYM2", "ZNF217", "ZNF266", "ZNF384"),
+                         MacrophageTB_Chaussabel_Blood_198_DN = c("ABCB11", "ACADSB", "ACSL1", "ADCYAP1R1", "AIM2", "AMHR2", "ANP32E", "ANPEP", "ARR3", "ARSF", "ATP13A3", "ATP2A2", "ATRX", "AVL9", "BAG2", "BCL2A1", "BECN1", "BICDL1", "BNIP3L", "C2orf72", "CCL19", "CCR7", "CD53", "CD96", "CDKN2C", "CELA2A", "CERS6", "CES1", "CFB", "CHP2", "COPS8", "CPA1", "CREB1", "CSNK1G2", "CTAG2", "CYP2C9", "DGCR5", "DLC1", "DLEC1", "DMTF1", "DNAJB4", "DR1", "DSC3", "DTNB", "DUS4L", "DVL3", "EDNRB", "ELAVL2", "ELF4", "F2RL3", "FCER1G", "FERMT2", "FRYL", "GADD45B", "GATC", "GBP2", "GNRH2", "GPX7", "GRB7", "GRIK5", "GRIP1", "GRPEL1", "GSK3A", "H2BC13", "HDAC2", "HIVEP1", "HNRNPH1", "HPS5", "HSPA6", "HSPB11", "IBTK", "ID1", "ID2", "IDO1", "IDS", "IFIH1", "IFRD1", "IL10RB", "IL16", "IL1RN", "INPP5B", "IQSEC2", "IRAK3", "IRF9", "KCNF1", "KCNK1", "KYNU", "LETMD1", "LINC01138", "LYZL6", "MAPKBP1", "MAT2A", "MBTPS1", "MCC", "MCM2", "MEF2D", "MIR483", "MKLN1", "MLLT10", "MTF1", "MTHFS", "MUC6", "MX2", "MYOC", "N4BP3", "NDUFC1", "NECTIN2", "NORAD", "NR5A2", "NTS", "NXF1", "OCLN", "OPRM1", "OPTN", "OSMR", "OVOL3", "PARD6B", "PBX2", "PDCL", "PDGFA", "PDHX", "PEPD", "PFDN4", "PGC", "PHC2", "PKN2", "PLK1", "PNP", "PPARD", "PPP1R3A", "PPWD1", "PRKACB", "PRKRA", "PROZ", "PRPH", "PRPSAP1", "PRRG1", "PTPN2", "PTPRCAP", "PUM3", "PYGM", "QDPR", "RAB2A", "RAP1GAP2", "RAP1GDS1", "RARA", "RELA", "REM1", "REPS1", "RFPL3S", "RHEB", "RPA3", "RRAS2", "RSAD2", "RTN4", "RUNX1", "SAP30", "SCAF8", "SEC22B", "SEMA4F", "SERPINE1", "SKP2", "SLC25A16", "SLC30A4", "SLC31A2", "SNRPF", "SOCS5", "SPINK4", "SPRED2", "SRP9", "SSB", "STAG2", "STAT1", "STAU1", "SUSD5", "TAF2", "TCF4", "TFDP2", "TFE3", "TGIF1", "TNFAIP1", "TNR", "TRIB1", "TRIM27", "TRIM38", "TRIP10", "TSC22D2", "TWIST1", "UBE2D1", "UBE2J1", "UBE2L3", "UBE2V2", "UCP3", "USP19", "VAC14", "VDAC2", "ZFYVE26", "ZNF189"),
+                         MacrophageTB_Chaussabel_Blood_198_UP = c("ACOT11", "ACRV1", "ADD1", "ADGRL1", "ADO", "AHCYL1", "ALDH1B1", "ANAPC5", "AP2S1", "AP3M2", "API5", "APPBP2", "ARF5", "ARRB2", "ARTN", "ASMTL", "ATXN2", "BANF1", "BRD3", "C1orf216", "CALD1", "CAMK1", "CBX7", "CCDC22", "CD59", "CD72", "CD74", "CDA", "CDIPT", "CDK11A", "CDK4", "CDR2L", "CEBPZ", "CHERP", "CLN3", "CLSTN1", "CMTR1", "COMMD4", "COPS6", "COX4I1", "CRYBB1", "CTSK", "CUL2", "CUX1", "CYB5R1", "CYP2B7P", "CYP2C8", "DAZAP2", "DAZL", "DCP2", "DHX38", "DNAH7", "DNAJC13", "DPM2", "DUSP3", "EEF1G", "ENOSF1", "ENTPD6", "ESD", "FAM149A", "FAM32A", "FARSA", "FCN1", "FMO6P", "FOXN3", "FRAT2", "GABARAP", "GARS1", "GBA", "GDI2", "GFUS", "GIT2", "GLIPR1", "GPC3", "GPNMB", "HIC2", "HLA-DRB4", "HSPB3", "KCTD12", "KDM4B", "KIF13B", "LAMB2", "LAMTOR5", "LILRB5", "LINC00847", "LIPA", "LMO2", "LPAR2", "LPL", "LTA4H", "LYRM9", "MAGI1", "MAK16", "MCM4", "MEF2C", "MIOS", "MISP", "MKNK2", "MLF2", "MRPL40", "MXI1", "MXRA8", "MYBPC2", "NAP1L4", "NBL1", "NDRG2", "NDST1", "NDUFV1", "NEDD8", "NEK2", "NPM1", "NREP", "NTNG1", "NUP188", "NUP214", "NUTF2", "OLFML2B", "OPA1", "ORC4", "PACS2", "PART1", "PC", "PCCB", "PCDHA12", "PCDHB11", "PFDN5", "PFKFB1", "PIP4K2B", "PLCG2", "PLOD3", "PLRG1", "PMM1", "POLR3C", "PPBPP2", "PPP2R5A", "PPP2R5E", "PRPF8", "PSMD1", "PTPN7", "PTPRD", "QKI", "QPRT", "RANBP1", "RCE1", "RHBDL1", "RPL5", "RPS4X", "RPS6", "RPS6KA1", "RPS6KB2", "RUVBL2", "S100A13", "SASH3", "SDC1", "SEMA7A", "SEPTIN2", "SERTAD2", "SF3A3", "SHC1", "SIRPB1", "SLC2A5", "SLC4A7", "SLC4A8", "SLC6A8", "SLC7A5", "SPC25", "SPECC1L", "SPINK2", "SPINT1", "SQSTM1", "SREBF2", "SSBP1", "SVIL", "TBC1D2B", "TCTA", "THUMPD1", "TLE5", "TNNC2", "TPP1", "TRAF5", "TRAK2", "TRAP1", "TRIM66", "TTC1", "UBXN1", "UQCRH", "URB2", "VAMP1", "VAMP2", "VAMP3", "VAMP8", "VAV1", "VOPP1", "XRCC2", "ZNF148", "ZNF629", "ZNHIT1", "ZYX"),
+                         MacrophageTB_Lavalett_BAL_33_UP = c("CHIT1", "CYP27B1", "MMP7", "CCL8", "CCND1", "MMP12", "CHI3L1", "RPS4Y1", "CRABP2", "GBP5", "UBD", "HLA-DQB1", "CXCL9", "FGL2", "GAL", "HLA-DRB4", "TSPAN32", "EIF1AY", "HLA-H", "CCND2", "IDO1", "PTGDS", "LTB", "GBP4", "MMP9", "STAT1", "SLC2A5", "CHST15", "HLA-C", "CCL22", "CCNA1", "CCL5", "TAP1"),
+                         MacrophageTB_Lavalett_BAL_18_DN = c("STMN1", "CAMP", "TGFB1", "JAKMIP2", "PLIN2", "MARCO", "HLA-DRB1", "CXCL5", "IL1B", "MCEMP1", "COLEC12", "CES1P1", "OLR1", "CD163", "AKR1C4", "AKR1C3", "AKR1C2", "CES1"),
+                         FEV1inCOPD_Becker_Bronchial_120_UP = c("TCN1", "AHCYL2", "MFSD4", "GALNT4", "PLA2G4A", "TM9SF3", "TSPAN13", "GALNT5", "GALNT7", "PARM1", "SURF4", "GALNT12", "ARMCX3", "RDH10", "CEACAM5", "GNPNAT1", "TSPAN8", "MAGT1", "PDXDC1", "LRRC8A", "FUT6", "ENTPD4", "SLC26A2", "S100A16", "SLC39A8", "TMEM165", "CTSC", "ASRGL1", "PTHLH", "ATP13A5", "ENTPD3", "CLDN10", "FER1L6", "PPAPDC1B", "AP2B1", "S100A14", "SCEL", "SEC31A", "GALE", "AZGP1", "NNT", "ARMCX6", "UPK1B", "PRSS23", "MORC4", "EIF2AK3", "TXNDC11", "SLC31A1", "S100P", "EPT1", "FAM177B", "WDR72", "CREB3L1", "HMGCS2", "MUC2", "FZD5", "TMPRSS4", "MIA3", "SLC44A3", "MTHFD2", "PRRC1", "VTCN1", "STK38L", "DISP1", "KDELR2", "SEC24A", "KCNK6", "RPN2", "ATP6V0E1", "MYO1C", "DGKA", "ACBD3", "FUT2", "B4GALT4", "CYP2C18", "VIPR1", "ANO10", "PRSS8", "MFSD1", "SLC1A5", "TSPAN5", "GNE", "TMEM39A", "TMEM167A", "IDH1", "C12orf23", "FUT3", "LOC100128816", "AP4B1", "PDK1", "SPTSSA", "SMPDL3A", "ADAM9", "SLC16A9", "SEC61A1", "ANP32E", "TMEM211", "CEACAM6", "OPN1LW", "SLC12A8", "FGFBP1", "SERPINB8", "CTTNBP2", "BCL2L15", "ALG14", "FKBP14", "DHX15", "MOB4", "TUBA1C", "PGBD2", "KCNK1", "MAL2", "PDIA5", "TIMP1", "GPT2", "TMED3", "NIPA2", "TPBG", "CANT1"),
+                         FEV1inCOPD_Becker_Bronchial_51_DN = c("KIF13A", "CCDC69", "PRKCE", "TBC1D22B", "BBS1", "FAM53B", "LINC00341", "MAML2", "SRGAP2", "CCDC170", "CNNM2", "WWC1", "CD38", "HIST3H2BB", "HEY2", "KCNB1", "C11orf63", "PCNT", "GAB2", "KIF24", "ZNF709", "CYP27A1", "ZNF382", "ZNF473", "BRF1", "FHAD1", "SCAI", "ZNF544", "SNRK", "CEP250", "USP2", "STXBP1", "ENO4", "RGAG4", "STPG1", "IL5RA", "ULK2", "ZBTB44", "NSUN7", "KCNJ2", "NEK4", "CEP104", "ZFP82", "NEK11", "CCDC151", "LAMC2", "ZNF391", "EZH1", "PCSK6", "PRSS12", "FXYD6", "PYGO1"))
+
+# Save rda file
+save(ImmuneSignatures, file = "ImmSigData.rda")
+
+# Create documentation
+# Go to data.R
+
+# https://compbiomed.github.io/TBSignatureProfiler-docs/articles/rmd/SignatureAddition.html
+mkTBreference <- function(TBsigname, ref, DOInum) {
+  cat("\\item{\\strong{", TBsigname, "}}{: ", ref,
+      " \\href{http://dx.doi.org/", DOInum, "}{", DOInum, "}}", sep = "")
+}
+
+mkTBreference(TBsigname = "LauxdaCosta2_OD_3", # The name output in a message from addTBsignature()
+              ref = "Laux da Costa L, Delcroix M, Dalla Costa ER, et al. A real-time PCR signature to discriminate between tuberculosis and other pulmonary diseases. Tuberculosis (Edinb). 2015;95(4):421-425.",
+              DOInum = "10.1016/j.tube.2015.04.008")
+
+"Huang YJ, Kim E, Cox MJ, et al. A persistent and diverse airway microbiota present during chronic obstructive pulmonary disease exacerbations. OMICS. 2010;14(1):9-59."
+
+
+# LOOKING AT ANNOT TABLE ------------------------------------------------------
+
+# AnnotImmuneData
+ImmuneAnnot <- 
+  matrix(c("DendriticTB_Chaussabel_Blood_200_DN", "TB", "Blood", "Dendrite",
+           "DendriticTB_Chaussabel_Blood_200_UP", "TB", "Blood", "Dendrite",
+           "FEV1inCOPD_Becker_Bronchial_51_DN", "COPD", "Respiratory", "Cell Type",
+           "FEV1inCOPD_Becker_Bronchial_120_UP", "COPD", "Respiratory", "Cell Type", 
+           "MacrophageTB_Chaussabel_Blood_198_DN", "TB", "Blood", "Macrophage",
+           "MacrophageTB_Chaussabel_Blood_198_UP", "TB", "Blood", "Macrophage",
+           "MacrophageTB_Lavalett_BAL_18_DN", "TB", "Respiratory", "Macrophage",
+           "MacrophageTB_Lavalett_BAL_33_UP", "TB", "Respiratory", "Macrophage"), ncol = 4,
+       byrow = TRUE, dimnames = list(c(), c("names", "disease", "tissue_type", "cell_type")))
+
+# Sort by the sig name
+
+# Save rda file as above
+save(ImmuneAnnot, file = "ImmSigAnnot.rda")
+
+# Make documentation
+
+#testing
+library(curatedTBData)
+library(SummarizedExperiment)
+library(TBSignatureProfiler)
+testdata <- curatedTBData("GSE31348", dry.run = FALSE, curated.only = FALSE)
+test <- testdata[[1]]
+test
+testinput <- MultiAssayExperiment::experiments(test)[["assay_curated"]]
+testoutput <- runTBsigProfiler(
+  testinput,
+  useAssay = NULL,
+  signatures = ImmuneSignatures,
+  algorithm = c("GSVA", "ssGSEA"), # "ASSIGN", "PLAGE", "Zscore", "singscore"),
+  combineSigAndAlgorithm = FALSE,
+  assignDir = NULL,
+  outputFormat = NULL,
+  parallel.sz = 0,
+  ASSIGNiter = 1e+05,
+  ASSIGNburnin = 50000,
+  ssgsea_norm = TRUE,
+  update_genes = FALSE)
+#curatedTBData("", dry.run = TRUE)
+testdata2 <- curatedTBData("GSE36238", dry.run = FALSE, curated.only = FALSE)
+test2 <- testdata2[[1]]
+test2
+testinput2 <- MultiAssayExperiment::experiments(test2)[["assay_curated"]]
+testoutput2 <- runTBsigProfiler(
+  testinput2,
+  useAssay = NULL,
+  signatures = ImmuneSignatures,
+  algorithm = c("GSVA", "ssGSEA"), # "ASSIGN", "PLAGE", "Zscore", "singscore"),
+  combineSigAndAlgorithm = FALSE,
+  assignDir = NULL,
+  outputFormat = NULL,
+  parallel.sz = 0,
+  ASSIGNiter = 1e+05,
+  ASSIGNburnin = 50000,
+  ssgsea_norm = TRUE,
+  update_genes = FALSE)
+
+latentactive <- curatedTBData("GSE19439", dry.run = FALSE, curated.only = FALSE)
+latact <- latentactive[[1]]
+latact
+latactinput <- MultiAssayExperiment::experiments(latact)[["assay_curated"]]
+latactoutput <- runTBsigProfiler(
+  latactinput,
+  useAssay = NULL,
+  signatures = ImmuneSignatures,
+  algorithm = c("GSVA", "ssGSEA"), # "ASSIGN", "PLAGE", "Zscore", "singscore"),
+  combineSigAndAlgorithm = FALSE,
+  assignDir = NULL,
+  outputFormat = NULL,
+  parallel.sz = 0,
+  ASSIGNiter = 1e+05,
+  ASSIGNburnin = 50000,
+  ssgsea_norm = TRUE,
+  update_genes = FALSE)
+
