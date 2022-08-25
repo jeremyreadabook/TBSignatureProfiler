6d0148adfe5f83d6f53f3cd542ff5bf8c159094f
jeremyreadabook <102390034+jeremyreadabook@users.noreply.github.com>
2022-08-25 12:48:46 -0600

    Update ImmuneSigsReactome.R

--- R/ImmuneSigsReactome.R
+++ R/ImmuneSigsReactome.R
@@ -1,16 +1,84 @@
 # Create signature 3
-ImmuneSignaturesReactome <- list()
+ImmuneSignaturesReactome <- list(Date8_15_Test_1	=	c("CFLAR", "BIRC3", "FAS", "CASP8", "XIAP", "TRADD", "STUB1", "TNFRSF10A", "BIRC2", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK3", "RIPK1", "OGT", "FADD")	,
+Date8_15_Test_2	=	c("H1-9P", "IGLV2-23", "RIPK3", "RIPK1")	,
+Date8_15_Test_3	=	c("RIPK3", "RIPK1")	,
+Date8_15_Test_4	=	c("RIPK3", "RIPK1", "MLKL")	,
+Date8_15_Test_5	=	c("RIPK3", "RIPK1", "MLKL", "H1-9P")	,
+Date8_15_Test_6	=	c("RIPK3", "FLOT2", "RIPK1", "FLOT1", "SDCBP", "MLKL", "PDCD6IP")	,
+Date8_15_Test_7	=	c("CFLAR", "BIRC3", "FAS", "CASP8", "HSP90AA1", "XIAP", "TRADD", "STUB1", "TNFRSF10A", "CDC37", "BIRC2", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK3", "FLOT2", "RIPK1", "FLOT1", "SDCBP", "RPS27A", "OGT", "UBC", "FADD", "MLKL", "PDCD6IP", "UBB", "PELI1", "UBA52")	,
+Date8_15_Test_8	=	c("CFLAR", "FAS", "CASP8", "TRADD", "TNFRSF10A", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK1", "FADD")	,
+Date8_15_Test_9	=	c("BIRC3", "XIAP", "BIRC2", "RPS27A", "UBC", "UBB", "UBA52")	,
+Date8_15_Test_10	=	c("CASP8", "TRADD", "TRAF2", "RIPK1", "FADD")	,
+Date8_15_Test_11	=	c("RIPK3", "CASP8")	,
+Date8_15_Test_12	=	c("RIPK3", "RIPK1", "PELI1")	,
+Date8_15_Test_13	=	c("RIPK3", "RIPK1", "RPS27A", "UBC", "UBB", "PELI1", "UBA52")	,
+Date8_15_Test_14	=	c("RIPK3", "OGT")	,
+Date8_15_Test_15	=	c("RIPK3", "HSP90AA1", "CDC37")	,
+Date8_15_Test_16	=	c("MLKL", "HSP90AA1", "CDC37")	,
+Date8_15_Test_17	=	c("RIPK3", "RIPK1", "MLKL", "FLOT1", "FLOT2")	,
+Date8_15_Test_18	=	c("RIPK3", "RIPK1", "MLKL", "SDCBP", "PDCP6IP")	,
+Date8_15_Test_19	=	c("RIPK3", "STUB1")	,
+Date8_15_Test_20	=	c("RIPK3", "STUB1", "UBB", "UBA52")	,
+Date8_15_Test_21	=	c("RIPK3", "RIPK1", "IGLV2-23")	,
+Date8_15_Test_22	=	c("RIPK3", "RIPK1", "MLKL", "CASP8")	,
+Date8_15_Test_23	=	c("BAK1", "TP63", "CHMP2B", "BAX", "GZMB", "CHMP4B", "GSDMD", "GSDME", "IL1A", "CHMP3", "IRF1", "IL1B", "CHMP2A", "CASP1", "CASP5", "TP53", "CHMP7", "IL18", "CASP3", "CHMP4C", "IRF2", "CYCS", "CHMP6", "HMGB1", "CASP4", "ELANE", "CHMP4A")	,
+Date8_15_Test_24	=	c("IRF2", "GSDMD", "IRF1")	,
+Date8_15_Test_25	=	c("TP63", "GSDMD", "TP53")	,
+Date8_15_Test_26	=	c("CASP1", "GSDMD", "CASP3")	,
+Date8_15_Test_27	=	c("ELANE", "GSDMD")	,
+Date8_15_Test_28	=	c("MGAT2", "GSDMD")	,
+Date8_15_Test_29	=	c("ACTL9", "GSDMD")	,
+Date8_15_Test_30	=	c("CASP3", "GSDMD")	,
+Date8_15_Test_31	=	c("CASP3", "GSDME")	,
+Date8_15_Test_32	=	c("GZMB", "GSDME")	,
+Date8_15_Test_33	=	c("ACTL9", "GSDME")	,
+Date8_15_Test_34	=	c("BAK1", "BAX", "GSDMD", "GSDME", "CYCS")	,
+Date8_15_Test_35	=	c("GSDMD", "IL1A", "IL1B", "IL18"))
 
 
 
 
 ## Add new signatures to the profiler (edited from the original new_micronutrients_code_sample.RMD)
 # ## Add new signatures to the profiler (in Arthurs Code) (edited from the original new_micronutrients_code_sample.RMD)
-
+TBsignatures$Date8_15_Test_1	<-	c("CFLAR", "BIRC3", "FAS", "CASP8", "XIAP", "TRADD", "STUB1", "TNFRSF10A", "BIRC2", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK3", "RIPK1", "OGT", "FADD")
+TBsignatures$Date8_15_Test_2	<-	c("H1-9P", "IGLV2-23", "RIPK3", "RIPK1")
+TBsignatures$Date8_15_Test_3	<-	c("RIPK3", "RIPK1")
+TBsignatures$Date8_15_Test_4	<-	c("RIPK3", "RIPK1", "MLKL")
+TBsignatures$Date8_15_Test_5	<-	c("RIPK3", "RIPK1", "MLKL", "H1-9P")
+TBsignatures$Date8_15_Test_6	<-	c("RIPK3", "FLOT2", "RIPK1", "FLOT1", "SDCBP", "MLKL", "PDCD6IP")
+TBsignatures$Date8_15_Test_7	<-	c("CFLAR", "BIRC3", "FAS", "CASP8", "HSP90AA1", "XIAP", "TRADD", "STUB1", "TNFRSF10A", "CDC37", "BIRC2", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK3", "FLOT2", "RIPK1", "FLOT1", "SDCBP", "RPS27A", "OGT", "UBC", "FADD", "MLKL", "PDCD6IP", "UBB", "PELI1", "UBA52")
+TBsignatures$Date8_15_Test_8	<-	c("CFLAR", "FAS", "CASP8", "TRADD", "TNFRSF10A", "FASLG", "TNFRSF10B", "TNFSF10", "TRAF2", "RIPK1", "FADD")
+TBsignatures$Date8_15_Test_9	<-	c("BIRC3", "XIAP", "BIRC2", "RPS27A", "UBC", "UBB", "UBA52")
+TBsignatures$Date8_15_Test_10	<-	c("CASP8", "TRADD", "TRAF2", "RIPK1", "FADD")
+TBsignatures$Date8_15_Test_11	<-	c("RIPK3", "CASP8")
+TBsignatures$Date8_15_Test_12	<-	c("RIPK3", "RIPK1", "PELI1")
+TBsignatures$Date8_15_Test_13	<-	c("RIPK3", "RIPK1", "RPS27A", "UBC", "UBB", "PELI1", "UBA52")
+TBsignatures$Date8_15_Test_14	<-	c("RIPK3", "OGT")
+TBsignatures$Date8_15_Test_15	<-	c("RIPK3", "HSP90AA1", "CDC37")
+TBsignatures$Date8_15_Test_16	<-	c("MLKL", "HSP90AA1", "CDC37")
+TBsignatures$Date8_15_Test_17	<-	c("RIPK3", "RIPK1", "MLKL", "FLOT1", "FLOT2")
+TBsignatures$Date8_15_Test_18	<-	c("RIPK3", "RIPK1", "MLKL", "SDCBP", "PDCP6IP")
+TBsignatures$Date8_15_Test_19	<-	c("RIPK3", "STUB1")
+TBsignatures$Date8_15_Test_20	<-	c("RIPK3", "STUB1", "UBB", "UBA52")
+TBsignatures$Date8_15_Test_21	<-	c("RIPK3", "RIPK1", "IGLV2-23")
+TBsignatures$Date8_15_Test_22	<-	c("RIPK3", "RIPK1", "MLKL", "CASP8")
+TBsignatures$Date8_15_Test_23	<-	c("BAK1", "TP63", "CHMP2B", "BAX", "GZMB", "CHMP4B", "GSDMD", "GSDME", "IL1A", "CHMP3", "IRF1", "IL1B", "CHMP2A", "CASP1", "CASP5", "TP53", "CHMP7", "IL18", "CASP3", "CHMP4C", "IRF2", "CYCS", "CHMP6", "HMGB1", "CASP4", "ELANE", "CHMP4A")
+TBsignatures$Date8_15_Test_24	<-	c("IRF2", "GSDMD", "IRF1")
+TBsignatures$Date8_15_Test_25	<-	c("TP63", "GSDMD", "TP53")
+TBsignatures$Date8_15_Test_26	<-	c("CASP1", "GSDMD", "CASP3")
+TBsignatures$Date8_15_Test_27	<-	c("ELANE", "GSDMD")
+TBsignatures$Date8_15_Test_28	<-	c("MGAT2", "GSDMD")
+TBsignatures$Date8_15_Test_29	<-	c("ACTL9", "GSDMD")
+TBsignatures$Date8_15_Test_30	<-	c("CASP3", "GSDMD")
+TBsignatures$Date8_15_Test_31	<-	c("CASP3", "GSDME")
+TBsignatures$Date8_15_Test_32	<-	c("GZMB", "GSDME")
+TBsignatures$Date8_15_Test_33	<-	c("ACTL9", "GSDME")
+TBsignatures$Date8_15_Test_34	<-	c("BAK1", "BAX", "GSDMD", "GSDME", "CYCS")
+TBsignatures$Date8_15_Test_35	<-	c("GSDMD", "IL1A", "IL1B", "IL18")
 
 #selected tb signatures
-samp_tbsignatures <- list()
-names(samp_tbsignatures) <- c()
+samp_tbsignatures <- list(TBsignatures$Date8_15_Test_1, TBsignatures$Date8_15_Test_2, TBsignatures$Date8_15_Test_3, TBsignatures$Date8_15_Test_4, TBsignatures$Date8_15_Test_5, TBsignatures$Date8_15_Test_6, TBsignatures$Date8_15_Test_7, TBsignatures$Date8_15_Test_8, TBsignatures$Date8_15_Test_9, TBsignatures$Date8_15_Test_10, TBsignatures$Date8_15_Test_11, TBsignatures$Date8_15_Test_12, TBsignatures$Date8_15_Test_13, TBsignatures$Date8_15_Test_14, TBsignatures$Date8_15_Test_15, TBsignatures$Date8_15_Test_16, TBsignatures$Date8_15_Test_17, TBsignatures$Date8_15_Test_18, TBsignatures$Date8_15_Test_19, TBsignatures$Date8_15_Test_20, TBsignatures$Date8_15_Test_21, TBsignatures$Date8_15_Test_22, TBsignatures$Date8_15_Test_23, TBsignatures$Date8_15_Test_24, TBsignatures$Date8_15_Test_25, TBsignatures$Date8_15_Test_26, TBsignatures$Date8_15_Test_27, TBsignatures$Date8_15_Test_28, TBsignatures$Date8_15_Test_29, TBsignatures$Date8_15_Test_30, TBsignatures$Date8_15_Test_31, TBsignatures$Date8_15_Test_32, TBsignatures$Date8_15_Test_33, TBsignatures$Date8_15_Test_34, TBsignatures$Date8_15_Test_35)
+names(samp_tbsignatures) <- c("Date8_15_Test_1", "Date8_15_Test_2", "Date8_15_Test_3", "Date8_15_Test_4", "Date8_15_Test_5", "Date8_15_Test_6", "Date8_15_Test_7", "Date8_15_Test_8", "Date8_15_Test_9", "Date8_15_Test_10", "Date8_15_Test_11", "Date8_15_Test_12", "Date8_15_Test_13", "Date8_15_Test_14", "Date8_15_Test_15", "Date8_15_Test_16", "Date8_15_Test_17", "Date8_15_Test_18", "Date8_15_Test_19", "Date8_15_Test_20", "Date8_15_Test_21", "Date8_15_Test_22", "Date8_15_Test_23", "Date8_15_Test_24", "Date8_15_Test_25", "Date8_15_Test_26", "Date8_15_Test_27", "Date8_15_Test_28", "Date8_15_Test_29", "Date8_15_Test_30", "Date8_15_Test_31", "Date8_15_Test_32", "Date8_15_Test_33", "Date8_15_Test_34", "Date8_15_Test_35")
 
 
 
